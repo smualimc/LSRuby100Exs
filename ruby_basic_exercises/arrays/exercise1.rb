abc7c90f1c85c95ffb104917e6f4ba19d1f7c944
smualimc <88353918+smualimc@users.noreply.github.com>
2022-02-12 09:49:20 +0100

    Arrays exercises

---
+++ ruby_basic_exercises/arrays/exercise1.rb
@@ -0,0 +1,7 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+# by selection => my_pet = pets.select {|e| e == 'fish'} 
+# by position =>
+my_pet = pets[2]
+
+puts my_pet
---
+++ ruby_basic_exercises/arrays/exercise6.rb
@@ -0,0 +1,3 @@
+numbers = [1, 2, 3, 4, 5]
+double_numbers = numbers.map {|number| number * 2}
+p double_numbers
